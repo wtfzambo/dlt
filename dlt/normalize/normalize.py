d72135f0816c24c4cac247614ae167b33aa57302
Dave <shrps@posteo.net>
2023-08-27 22:31:43 +0200

    small changes

--- dlt/normalize/normalize.py
+++ dlt/normalize/normalize.py
@@ -19,6 +19,7 @@ from dlt.common.typing import TDataItem
 from dlt.common.schema import TSchemaUpdate, Schema
 from dlt.common.schema.exceptions import CannotCoerceColumnException
 from dlt.common.utils import chunks
+from dlt.normalize.exceptions import SchemaFrozenException
 
 from dlt.normalize.configuration import NormalizeConfiguration
 
@@ -150,7 +151,7 @@ class Normalize(Runnable[ProcessPool]):
 
                     # if there is a schema update and we disallow any data not fitting the schema, raise!
                     elif partial_table and config.schema_update_mode == "freeze-and-raise":
-                        raise Exception("Schema frozen!")
+                        raise SchemaFrozenException(f"Trying to modify table {table_name} but schema is frozen.")
 
                     # theres a new table or new columns in existing table
                     elif partial_table:
